763fea8a696c16c9f97356d6db2851d9e6646575
Foysal Ahmed <79821057+foysal90@users.noreply.github.com>
2021-05-19 22:24:35 -0400

    modified setting.py

--- twitter/settings.py
+++ twitter/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = 'django-insecure-kf#*7@yam482@duh!fx85fa7#(h8gv-zg!!lr2ei=98ctlpv*e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['rocky-citadel-40633.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = [ "*" ]
+ 
+#  ['rocky-citadel-40633.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
